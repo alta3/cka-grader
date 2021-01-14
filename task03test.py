f65aae94b4de68f4df219d3f950458259dfe77c9
Sam Griffith <47530477+sgriffith3@users.noreply.github.com>
2021-01-14 11:35:37 -0500

    Update task03test.py

--- task03test.py
+++ task03test.py
@@ -2,12 +2,12 @@ import subprocess as sp
 
 def task03(pp=9):
     points = 0
-    cmd01 = 'ssh master-01 kubectl get nodes'
+    cmd01 = 'ssh master-1 kubectl get nodes'
     out = sp.Popen(cmd01, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT, close_fds=True)
     txt = out.stdout.read().decode('utf-8')
     lines = txt.splitlines()
     for line in lines:
-        if line.strip().startswith("master-01"):
+        if line.strip().startswith("master-1"):
             points += 7
             if line.strip().endswith("v1.19.0"):
                 points += 2
