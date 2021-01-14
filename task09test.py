79673b631d93a90d3e1ffc104920b9ef44865e59
Sam Griffith <47530477+sgriffith3@users.noreply.github.com>
2021-01-14 12:04:03 -0500

    Update task09test.py

--- task09test.py
+++ task09test.py
@@ -2,7 +2,7 @@ import subprocess as sp
 
 def task09(pp=7):
     points = 0
-    cmd01 = 'ssh master-03 kubectl get pods'
+    cmd01 = 'ssh master-3 kubectl get pods'
     out = sp.Popen(cmd01, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT, close_fds=True)
     txt = out.stdout.read().decode('utf-8')
     lines = txt.splitlines()
@@ -10,7 +10,7 @@ def task09(pp=7):
         line = line.strip()
         if line.startswith("isitback"):    
             points += 4
-    cmd02 = 'ssh master-03 ls /opt/'
+    cmd02 = 'ssh master-3 ls /opt/'
     out02 = sp.Popen(cmd02, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT, close_fds=True)
     txt02 = out02.stdout.read().decode('utf-8')
     lines02 = txt02.splitlines()
